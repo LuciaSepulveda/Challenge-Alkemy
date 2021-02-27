e08fcbc458d5fb955c2139d2add2012bcd9e9e8a
LuciaBSep <luciabsep18@gmail.com>
2021-02-27 11:01:29 -0300

    first commit

---
+++ src/App/App.tsx
@@ -0,0 +1,8 @@
+import {Box} from "@chakra-ui/react"
+import * as React from "react"
+
+const App: React.FC = () => {
+  return <Box bg="gray.400" w="100%" />
+}
+
+export default App
---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import {ChakraProvider} from "@chakra-ui/react"
+
+import theme from "./theme"
+import App from "./App"
+import "./theme.css"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>,
+  document.getElementById("root"),
+)
